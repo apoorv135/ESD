a834d3ccbaebfbe4eb16f8fa2b9f8c92b7cf924d
Apoorv Panse <apoorv135@gmail.com>
2021-11-16 05:14:54 +0530

    commit

---
+++ demo/src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Lab1");
+        System.out.println("HelloWorld");
+    }
+}
